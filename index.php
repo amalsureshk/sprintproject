6e43d1af6fc3e434c29917243db42dfe8511efa4
amalsureshk <amalsureshk@gmail.com>
2017-06-07 15:41:44 +0530

    project1

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/indexstyle.css">
+	<script src="javascript/jquery-3.1.1.min.js"></script>
+	<script src="javascript/bootstrap.min.js"></script>
+	<script src="javascript/jquery.min.js"></script>
+    <script type="text/javascript" src="javascript/scrit.js"></script>
+    <script>
+    </script>
+    
+ 
+   
+
+</head>
+
+<body>
+<div class="container-fluid" >
+		<div id="header" >
+		 <div class="row" >
+			<div class="col-md-5"  style="background-color: #3b5998;height: 120px">
+				<img src="image/images.png">
+			</div>
+			
+			<div class="col-md-3 hidden-xs" style="background-color: #3b5998;height: 120px">
+
+			    <p><font color="white">Email or phone</font></p>
+				<input type="text" name="username" id="username" placeholder="hari@gmail.com" class="form-control"><br>
+				<input type="checkbox" name="ed" value="Log-in" checked><font color=" white" >keep me logged in</font>
+			</div>
+			<div class="col-md-2 hidden-xs" style="background-color: #3b5998;height: 120px">
+			<p><font color="white">password</font></p>
+			<input type="password" name="password" id="password" placeholder="" class="form-control">
+			<p><font color="white">forgotten your password</font></p>
+			</div>
+			<div class="col-md-2 hidden-xs" style="background-color: #3b5998;height: 120px">
+
+			<button class="button" id="login" style="background-color: #3b5998;"><font color="white">login</font></button></div>
+			</div>
+			</div>
+			</div>
+			
+			<div class=section>
+			<div class="row hidden-xs">
+				<div class="col-md-6" style="background-color:#dfe3ee;height: 550px">
+					<img src="image/logs.jpg" id="left" height="43%">
+				</div>
+				<div class="col-md-6" style="background-color:#dfe3ee;height: 550px;">
+				<div class="row" id=right>
+			
+					<h1>create an account</h1>
+				<p>its free and always will be</p>
+			  <div class="col-md-6" style="background-color:#dfe3ee;">
+			   <input style="padding: 6px ;" type="text" name="wsjh" placeholder="firstname" class="form-control">
+			</div>
+			    <div class="col-md-6" style="background-color:#dfe3ee;">
+				<input style="padding: 6px;" type="text" name="xde" placeholder="secondname" class="form-control">
+			    </div>
+			    <div class="col-md-12" style="background-color:#dfe3ee;">
+				<input style="padding: 6px;" type="text" name="ed" placeholder="EMAIL or mobilenumber" class="form-control" size="49">
+			
+				<input style="padding: 6px;" type="text" name="edd" placeholder="RE-enter mobile or phone number" class="form-control" size="49">
+			
+				<input style="padding: 6px;" type="password" name="xwx" placeholder="New password" class="form-control" size="49">
+			   </div>
+			<p>Birthday</p>
+			
+			<select >
+				<option>day</option>
+				<option>monday</option>
+				<option>tuesday</option>
+			</select>
+			<select>
+			<option>month</option>
+			<option>januvary</option>
+			<option>februvary</option>
+
+			</select>
+			<select>
+				<option >year</option>
+				<option>1990</option>
+				<option>1991</option>
+				<option>2005</option>
+				
+			</select><br>
+			
+		<div id="box">
+			why do i need to provide DOB
+		</div><br>
+
+		<div id=gen>
+			
+			
+			<input type="radio" name="wd"><label>female</label>
+		    <input type="radio" name="ftyy"><label>male</label>
+
+			<p>by clicking to link you agree to our <font color="blue">
+			terms</font> and that<br> you have tp read our <font color="blue"> data policy</font>.including our <font color="blue">cookie use.</font></p>
+			</div>
+			<div class=butt>
+			   <p><button class="button" style="background-color:#006600;height:40px; width:150px;"><font color="white">create an account  </font></button>
+			</div>
+				</div>
+			</div>
+			</div>	
+			</div>
+			</div>
+			
+			<div class="container-fluid">
+			<div id="mobileheader">
+			<div class="row" id="lleft">
+				<div class="col-xs-12 hidden-lg" >
+				
+				</div>
+				<div class="col-xs-12 hidden-lg" >
+				<input type="text" name="csd" placeholder="email or phone number" class="form-control" style="background-color: #dfe3ee;">
+				<input type="password" name="csd" placeholder="password" class="form-control" style="background-color: #dfe3ee;">
+				<button type="login" class="form-control" style="background-color: #3b5998;"><font color="white"> login</button>
+				<div class=butt>
+			   <p><button class="button" id="las" style="background-color: #0066cc;height:40px; width:150px;"><font color="white" >create an account  </font></button>
+
+            </div>
+			</div>
+				
+			</div>
+</div>
+</body>
+</html>			
